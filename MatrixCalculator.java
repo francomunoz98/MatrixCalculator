18f0b8a28585c2141cd574ebfc184565eee502b3
francomunoz98 <63399725+francomunoz98@users.noreply.github.com>
2020-04-09 14:34:43 +0200

    Started MatrixCalculator.java and created starting Variables

---
+++ MatrixCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MatrixCalculator {
+
+	int[][] matA, matB, lösung;
+	int sum, dif;
+
+	public MatrixCalculator() {
+
+		matA = new int [2][2];
+		matB = new int [2][2];
+		lösung = new int [2][2];
+		sum = 0;
+		dif = 0;
+	}
